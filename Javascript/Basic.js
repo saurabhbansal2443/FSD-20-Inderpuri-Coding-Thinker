f96bd4bf1ddb4d8b54ac0fbe4ff35bf6a3a078c7
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-06-02 19:45:09 +0530

    Primitive data types complete

---
+++ Javascript/Basic.js
@@ -0,0 +1,30 @@
+// Print Statements
+
+console.log("Hello World");
+
+// Data Types
+// Primitive / non-primitive(Objects)
+
+//   Primitive Data Type  - String , boolean , number , null , undefined
+// Non primitive Data types -- objects ---(Hashmap(obejct) , Array )
+
+//  Var let and const
+
+// let a = 's'
+// console.log(typeof(a)); DataType is String
+
+// let n = 10.6
+// console.log(typeof(n)) DataType is number
+
+// let x = false;
+
+// console.log(typeof x);DataType is boolean
+
+// Undefined and null both states absence of value/data
+
+// Null signifies absence of value intentionally 
+// undefine absence of value 
+
+let x = null ;
+
+console.log(x)
