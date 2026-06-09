e5f6e028ed1f4a515021967567c6c15c9bf21fad
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-06-09 19:26:35 +0530

    Var let an d const started

---
+++ Javascript/VarVsLetVsConst.js
@@ -0,0 +1,17 @@
+//  Var vs Let vs Const
+
+// Redeclaration -- Var can be redeclared  , let and const cannot be redecalred
+// const a = 10;
+// const  a = 100;
+// console.log(a);
+
+// Reinitialization --> var, let  can be reintialise , const cannot be reintialize 
+// const a ; 
+// a = 10 ; 
+// console.log(a); 
+
+// Reassigning --> const cannot be reassign , var and let can be re-assign  
+
+var a = 10 ; 
+a = 20 ; 
+console.log(a); 
