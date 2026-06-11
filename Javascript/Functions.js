ac6dd40414d9e521d8225d37ad900e5a57dcea92
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-06-11 20:23:47 +0530

    Var vs let vs const

---
+++ Javascript/Functions.js
@@ -0,0 +1,34 @@
+// Functions -- are block of code to perform a specific task
+
+// function factorial(n) {
+//   if (n == 0 || n == 1) {
+//     return 1;
+//   }
+
+//   let ans = 1;
+
+//   for (let i = 2; i <= n; i++) {
+//     ans = ans * i;
+//   }
+
+//   return ans;
+// }
+
+// let ans = factorial(5);
+
+// console.log(ans);
+
+// function greet(){
+//     console.log("Good evening")
+//     // return 6 ; 
+// }
+// let ans = greet()
+// console.log(ans)
+
+let greet = function(){
+    console.log("Hey Good Evening"); 
+}
+
+greet(); 
+
+
---
+++ Javascript/varLetConst.js
@@ -0,0 +1,54 @@
+//  Var vs Let vs Const
+
+// Redeclaration -- Var can be redeclared  , let and const cannot be redecalred
+// const a = 10;
+// const  a = 100;
+// console.log(a);
+
+// Reinitialization --> var, let  can be reintialise , const cannot be reintialize
+// const a ;
+// a = 10 ;
+// console.log(a);
+
+// Reassigning --> const cannot be reassign , var and let can be re-assign
+
+// var a = 10 ;
+// a = 20 ;
+// console.log(a);
+
+// Function is a block of code to do a specific task
+
+// Hoisting -->  The variable and function will get the memory before the actual code execution ;
+// greet()
+
+// function greet() {
+//   console.log("Good Evening");
+// }
+
+// var let and const are hoisted, but let and const cannot be accessed before intialixation
+
+// console.log(a); // undefined
+// var a = 10;
+// console.log(a); // 10
+
+// console.log(a); // error
+// let a ;
+// console.log(a); // 10
+one();
+console.log(a);
+var a = 10;
+var b = 100;
+console.log(a);
+one();
+
+function one() {
+  console.log(a);
+  var a = 100;
+  console.log(a);
+  console.log(b);
+  function two() {
+    var b = 99;
+    console.log(b);
+  }
+  two();
+}
