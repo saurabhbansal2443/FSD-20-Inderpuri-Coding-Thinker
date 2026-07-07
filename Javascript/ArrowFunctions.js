c394b61be542e242586c26e34b3767d075e26f8a
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-07 20:29:39 +0530

    Arrow functions and IIFE

---
+++ Javascript/ArrowFunctions.js
@@ -0,0 +1,18 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // let add2 = (a, b) => {
+// //   return a + b;
+// // };
+
+// let add2 = (a, b) => a + b;
+
+// console.log(add(2, 3));
+// console.log(add2(2, 3));
+
+//IIFE -- Immedeitly invoked function expression
+
+(function greet() {
+  console.log("Hellos");
+})();
