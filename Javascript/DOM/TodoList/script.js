f5f09200637fa6861702af3851a7ddff1d88338d
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-16 20:21:52 +0530

    task 2 of todo list complete

---
+++ Javascript/DOM/TodoList/script.js
@@ -0,0 +1,26 @@
+const inputTag = document.getElementById("input");
+const buttonTag = document.getElementById("btn");
+const containerList = document.querySelector(".containerList");
+
+buttonTag.addEventListener("click", function () {
+  const task = inputTag.value;
+  //   console.log(task);
+  const taskBox = document.createElement("div");
+  taskBox.classList.add("task");
+  taskBox.innerHTML = `
+   <p>${task}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          fill="currentColor"
+        >
+          <path
+            d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
+          ></path>
+        </svg>`;
+
+  containerList.appendChild(taskBox);
+  console.log(taskBox);
+});
