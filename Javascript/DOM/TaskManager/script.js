c0cdd694aa243134a9f227735538c915d9908dc4
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-01 20:05:47 +0530

    Getting task adder text

--- Javascript/DOM/TaskManager/script.js
+++ Javascript/DOM/TaskManager/script.js
@@ -1,6 +1,10 @@
 const addButton = document.getElementById("add");
 const taskAdderContainer = document.querySelector(".taskAdder");
-
+// add task button functionality 
 addButton.addEventListener("click", function () {
   taskAdderContainer.classList.toggle("hide");
 });
+
+
+// get the task text from task Adder 
+// Get the color from taskAdder 
