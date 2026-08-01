417820676b97d83601c601a199caa293d25c6c28
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-08-01 20:19:37 +0530

    Get the text from task Area

--- Javascript/DOM/TaskManager/script.js
+++ Javascript/DOM/TaskManager/script.js
@@ -1,10 +1,21 @@
 const addButton = document.getElementById("add");
 const taskAdderContainer = document.querySelector(".taskAdder");
-// add task button functionality 
+const taskTextArea = document.querySelector(".taskText");
+
+// add task button functionality
 addButton.addEventListener("click", function () {
   taskAdderContainer.classList.toggle("hide");
 });
 
+// get the task text from task Adder
+// Get the color from taskAdder
 
-// get the task text from task Adder 
-// Get the color from taskAdder 
+taskTextArea.addEventListener("keydown", function (event) {
+  const key = event.key;
+  if (key != "Enter") {
+    return;
+  }
+
+  const taskText = taskTextArea.value;
+  console.log(taskText);
+});
