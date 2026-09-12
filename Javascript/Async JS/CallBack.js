8796b74e00015ce010e980a72bba0af93164a8f6
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-09-12 20:15:55 +0530

    Promises 1 Step done

--- Javascript/Async JS/CallBack.js
+++ Javascript/Async JS/CallBack.js
@@ -1,5 +1,40 @@
-console.log("First");
-setTimeout(function () {
-  console.log("Timeout called");
-}, 2000);
-console.log("Last");
+// console.log("First");
+// setTimeout(function () {
+//   console.log("Timeout called");
+// }, 2000);
+// console.log("Last");
+
+// Cut The Bread
+// Add Stuffings
+// Bake the sandwich
+
+function cutTheBread(cb) {
+  setTimeout(function () {
+    let bread = "bread";
+    console.log(bread);
+    cb();
+  }, 2000);
+}
+
+function addStuffings(cb) {
+  setTimeout(function () {
+    let sand = "bread + Stuffing";
+    console.log(sand);
+    cb();
+  }, 2000);
+}
+
+function bake() {
+  setTimeout(function () {
+    let sandWich = "bread + Stuffing + heat";
+    console.log(sandWich);
+  }, 2000);
+}
+
+// cutTheBread(addStuffings); // f
+
+cutTheBread(function () {
+  addStuffings(function () {
+    bake();
+  });
+});
